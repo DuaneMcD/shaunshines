27de87155889279e30065ae62dabeb88edb4bfb8
Duane McDonald <mcdonald.duane13@gmail.com>
2021-04-07 20:16:24 -0400

    Schedule service WIP -car selectors

---
+++ schedule-service/service.js
@@ -0,0 +1,15 @@
+// function loadYears() {
+//     const url = "https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getYears";
+//     let carYears = document.querySelector(".year").innerHTML;
+//     carYears += `<option value='robot'>Robot Car</option> `
+
+//     fetch(url)
+//         .then(function (response) {
+//         return response.text();
+//         })
+//         .then(function (body) {
+//             console.log(body.text());
+//         });
+    
+// }
+// loadYears();
